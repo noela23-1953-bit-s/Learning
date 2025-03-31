d0d85f42f8eabdfe6171862b8c19d9beb851a88f
noela23-1953-bit-s <gracianoela2@gmail.com>
2025-03-31 14:46:47 +0300

    commit#1
    in the NV on line 17, Euro was changed to UGx.

--- Task9/ExchangeRate/src/test/java/ExchangeRate_Package/ExchangeRateTest.java
+++ Task9/ExchangeRate/src/test/java/ExchangeRate_Package/ExchangeRateTest.java
@@ -14,7 +14,7 @@ class ExchangeRateTest {
     }
 
     @Test
-    void testGetExchangeRate_PoundToEuro() {
+    void testGetExchangeRate_PoundToUGx() {
         double expectedRate = 1.20;
         double actualRate = exchangeRate.getExchangeRate("POUND", "EURO");
         assertEquals(expectedRate, actualRate, 0.001, "POUND to EURO conversion rate should be 1.20");
