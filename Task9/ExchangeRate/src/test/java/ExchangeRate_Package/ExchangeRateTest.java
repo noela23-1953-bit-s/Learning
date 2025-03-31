20192e153e747c8cab2a6baf1e9a2e3a8664d386
noela23-1953-bit-s <gracianoela2@gmail.com>
2025-03-31 13:31:28 +0300

    Add files via upload

---
+++ Task9/ExchangeRate/src/test/java/ExchangeRate_Package/ExchangeRateTest.java
@@ -0,0 +1,38 @@
+package ExchangeRate_Package;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExchangeRateTest {
+    private ExchangeRate exchangeRate;
+
+    @BeforeEach
+    void setUp() {
+        exchangeRate = new ExchangeRate();
+    }
+
+    @Test
+    void testGetExchangeRate_PoundToEuro() {
+        double expectedRate = 1.20;
+        double actualRate = exchangeRate.getExchangeRate("POUND", "EURO");
+        assertEquals(expectedRate, actualRate, 0.001, "POUND to EURO conversion rate should be 1.20");
+    }
+
+    @Test
+    void testGetExchangeRate_UsdToUgx() {
+        assertEquals(3630, exchangeRate.getExchangeRate("USD", "UGX"),
+                "USD to UGX conversion rate should be 3630");
+    }
+
+    @Test
+    void testGetExchangeRate_NariaToDollar() {
+        assertEquals(1539.42, exchangeRate.getExchangeRate("NARIA", "DOLLAR"), 0.001,
+                "NARIA to DOLLAR conversion is correct");
+    }
+
+    @Test
+    void testGetExchangeRate_InvalidCurrency() {
+       assertEquals(1.20,exchangeRate.getExchangeRate("POUND","BIT COINE"));
+    }
+}
---
+++ Task9/Main_module/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module Main.module {
+    exports Main_Package;
+    requires ExchangeRate_Package;
+}
